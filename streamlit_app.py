16e2ca4035c7a80053d8abb70b6d39d6d103b9b8
steven-shoemaker <steven@stevenshoemaker.me>
2021-01-10 19:43:51 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,9 +20,9 @@ def remote_css(url):
     st.markdown(f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True)
 
 local_css("style.css")
-tokenizer = AutoTokenizer.from_pretrained("stevenshoemaker/horror")
+tokenizer = AutoTokenizer.from_pretrained("stevenshoemaker/horror/")
 
-model = AutoModelWithLMHead.from_pretrained("stevenshoemaker/horror")
+model = AutoModelWithLMHead.from_pretrained("stevenshoemaker/horror/")
 device = torch.device("cpu")
 
 model = model.to(device)
