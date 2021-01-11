0f938e500acbd38132ce161c972030bf0d8fbeac
steven-shoemaker <steven@stevenshoemaker.me>
2021-01-10 22:28:21 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,17 @@ import random
 import streamlit as st
 from transformers import AutoTokenizer, AutoModelWithLMHead
 
+
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+def remote_css(url):
+    st.markdown(f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True)
+
+local_css("style.css")
+
+
 desc = "Uses a neural network trained on over *5000* horror movies to generate sometimes good, *mostly non-sensical* horror movie plots after being given a movie title. This program attempts it's best guess at generating a movie based on whatever title you give it. "
 
 st.title('The Pitch Doctor')
