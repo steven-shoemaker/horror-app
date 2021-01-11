e220fe392a175f080017ac91c2f553d69e8bfd56
steven-shoemaker <steven@stevenshoemaker.me>
2021-01-10 19:48:41 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,9 +20,9 @@ def remote_css(url):
     st.markdown(f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True)
 
 local_css("style.css")
-tokenizer = AutoTokenizer.from_pretrained("abbas/gpt2-horror-stories")
+tokenizer = AutoTokenizer.from_pretrained("stevenshoemaker/horror")
 
-model = AutoModelWithLMHead.from_pretrained("abbas/gpt2-horror-stories")
+model = AutoModelWithLMHead.from_pretrained("stevenshoemaker/horror")
 device = torch.device("cpu")
 
 model = model.to(device)
