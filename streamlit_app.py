55bc04b5d70072f86baf39fe4195ddb45dd9a09e
steven-shoemaker <steven@stevenshoemaker.me>
2021-01-21 15:40:41 -0600

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+from nltk.tokenize import sent_tokenize 
 
 def local_css(file_name):
     with open(file_name) as f:
@@ -17,12 +17,18 @@ st.markdown("<b>Note, this app is still in-development so you may receive cut of
 st.write(desc)
 
 st.subheader("Enter the name of your film and hit enter:")
-prompt = st.text_input("") + " is a"
+prompt = st.text_input("") + " is about"
 
 import requests
 import json
 import time
+import re
 payload = json.dumps(prompt)
+
+from nltk.tokenize.punkt import PunktSentenceTokenizer, PunktParameters
+punkt_param = PunktParameters()
+punkt_param.abbrev_types = set(['dr', 'vs', 'mr', 'mrs', 'prof', 'inc'])
+sentence_splitter = PunktSentenceTokenizer(punkt_param)
 
 
 API_URL = "https://api-inference.huggingface.co/models/stevenshoemaker/horror"
@@ -33,8 +39,9 @@ if st.button('Scare Me'):
           headers = {"Content-Type": "application/json", "Authorization": "Bearer <YOUR_API_KEY>"}
           response = requests.post(API_URL, payload, headers=headers)
           movie = response.json()[0]["generated_text"]
-          st.subheader(prompt[:-4]) 
-          st.write(movie)
+          st.subheader(prompt[:-8]) 
+          movie = sentence_splitter.tokenize(movie)[:-1]
+          st.markdown(' '.join(word for word in movie))
      except: 
          st.write("Our servers are dusting off some cobwebs, can you please try your response again or use a different movie name?")
   
